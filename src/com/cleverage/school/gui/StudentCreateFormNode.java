65b091f066bf71f565c64945695acbd993f45882
David OLMETA <dolmeta@clever-age.com>
2014-10-24 07:33:53 +0200

    Add the form component of student creation

---
+++ src/com/cleverage/school/gui/StudentCreateFormNode.java
@@ -0,0 +1,99 @@
+package com.cleverage.school.gui;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+
+import com.cleverage.school.model.Student;
+import com.cleverage.school.service.StudentService;
+import com.cleverage.school.spring.SchoolSpring;
+
+
+/**
+ * Allows to display a form to create a new student.
+ */
+public class StudentCreateFormNode extends Parent
+{
+	private final TextField firstname;
+	private final TextField lastname;
+	private final DatePicker dateOfBirth;
+
+	/**
+	 * Default constructor.
+	 */
+	public StudentCreateFormNode()
+	{
+		// Create components.
+		this.firstname = new TextField();
+		this.lastname = new TextField();
+		this.dateOfBirth = new DatePicker();
+
+		// Create grid and get children.
+		final GridPane gridPane = new GridPane();
+		gridPane.setHgap(4);
+		gridPane.setVgap(6);
+		gridPane.setPadding(new Insets(18, 18, 18, 18));
+		final ObservableList<Node> components = gridPane.getChildren();
+
+		// Add first name line.
+		final Label firstnameLabel = new Label("Firstname:");
+		GridPane.setConstraints(firstnameLabel, 0, 0);
+		components.add(firstnameLabel);
+
+		GridPane.setConstraints(firstname, 1, 0);
+		components.add(firstname);
+
+		// Add last name line.
+		final Label lastnameLabel = new Label("Lastname:");
+		GridPane.setConstraints(lastnameLabel, 0, 1);
+		components.add(lastnameLabel);
+
+		GridPane.setConstraints(lastname, 1, 1);
+		components.add(lastname);
+
+		// Add the date of birth line.
+		final Label dobLabel = new Label("Date Of Birth:");
+		GridPane.setConstraints(dobLabel, 0, 2);
+		components.add(dobLabel);
+
+		GridPane.setConstraints(dateOfBirth, 1, 2);
+		components.add(dateOfBirth);
+
+		// Add the button.
+		final Button button = new Button("Save");
+		button.setOnAction(new EventHandler<ActionEvent>()
+		{
+			@Override
+			public void handle(final ActionEvent event)
+			{
+				// Create a student.
+				final LocalDate localDate = dateOfBirth.getValue();
+				final Instant instant = Instant.from(localDate.atStartOfDay(ZoneId.systemDefault()));
+				final Student student = new Student(firstname.getText(), lastname.getText(), Date.from(instant));
+
+				// Get the bean and save the student.
+				final StudentService studentService = (StudentService) SchoolSpring.getInstance().getBean("studentService");
+				studentService.saveStudent(student);
+			}
+		});
+
+		GridPane.setConstraints(button, 0, 3);
+		components.add(button);
+
+		getChildren().addAll(gridPane);
+	}
+
+}
